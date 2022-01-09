5ccd2517312fe339e5b8df144dd7ab732d8382f3
mamenglu <2245468037@qq.com>
2022-01-09 22:20:22 +0800

    init

---
+++ precache-manifest.8029c56bec7583cd528ccb75dcdfecb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8de56aad12c22bd4321cd7b1dc35de",
+    "url": "/Account-website/index.html"
+  },
+  {
+    "revision": "02d4f21e73c3321166d2",
+    "url": "/Account-website/static/css/main.cad8e927.chunk.css"
+  },
+  {
+    "revision": "bb13b43ef1239cc96bba",
+    "url": "/Account-website/static/js/2.ce115c84.chunk.js"
+  },
+  {
+    "revision": "be967568c01e0a277d271df321c0f0d8",
+    "url": "/Account-website/static/js/2.ce115c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d4f21e73c3321166d2",
+    "url": "/Account-website/static/js/main.d72180eb.chunk.js"
+  },
+  {
+    "revision": "b88c248bf676c3c80924",
+    "url": "/Account-website/static/js/runtime-main.e660353a.js"
+  }
+]);
